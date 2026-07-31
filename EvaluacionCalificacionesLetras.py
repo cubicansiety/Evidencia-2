314b736174c59e00a0f9f4813a2ec594ec7d3aa8
GONZALEZ LEGAZPI LUIS ALFONSO <sosrryidontknow@gmail.com>
2026-07-31 15:29:17 -0600

    Add files via upload

---
+++ EvaluacionCalificacionesLetras.py
@@ -0,0 +1,11 @@
+cali=float(input("Ingrese su calificacion entre 0-100: "))
+if cali>=90:
+    print("Su calificacion es de: A")
+elif cali>=80:
+    print("Su calificacion es de: B")
+elif cali>=70:
+    print("Su calificacion es de: C")
+elif cali>=60:
+    print("Su calificacion es de: D")
+else:
+    print("Su calificacion es de: F")
